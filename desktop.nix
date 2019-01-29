5edd9f7e7021b7f9a0c11307050b1af80e149b20
Aksel Skogsberg <xskoak@gmail.com>
2019-01-29 06:27:09 +0100

    Added murmur to desktop

--- desktop.nix
+++ desktop.nix
@@ -3,4 +3,7 @@
 {
   boot.loader.grub.device = "/dev/sda";
   networking.hostName = "tranquility";
+  services.murmur.bandwidth = 128000;
+  services.murmur.enable = true;
+  services.murmur.password = crampbutt;
 }
